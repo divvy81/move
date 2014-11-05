345a576726bf9e12c09855aef35f3c7b9896355c
divvy81 <divvy81@gmail.com>
2014-11-06 03:17:24 +0530

    highscore is displayed

--- Runner/Assets/Scripts/highscore.cs
+++ Runner/Assets/Scripts/highscore.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Net;
+using System.IO;
+using System.Text;
 
 
 public class highscore : MonoBehaviour {
@@ -12,7 +15,38 @@ public class highscore : MonoBehaviour {
 	public Font font;
 	public GUIStyle mystyle;
 	public GUISkin mystyle1;
+	string highscore_url= "http://127.0.0.1/highscore.php";
+	int[] previous_score = new int[ 40 ];
+	string label ="";
+	string[] Line = new string[40];
 
+
+
+	void Start()
+	{
+		getHighScore ();
+	}
+	void getHighScore()
+	{
+		string highscore_url = this.highscore_url + "?username=" + hscontroller.username;
+		HttpWebRequest connection =
+			(HttpWebRequest)HttpWebRequest.Create(highscore_url);
+		
+		connection.Method = "GET";
+		HttpWebResponse response =
+			(HttpWebResponse)connection.GetResponse();
+		//for(i=0;i<5;i++)
+		
+		StreamReader sr =new StreamReader(response.GetResponseStream(),Encoding.UTF8);
+		{
+			for(int i=0;i<34;i++)
+			{
+				Line[i]=sr.ReadLine();
+				//Debug.Log ("the value is "+Line[i]);
+			}
+		}
+
+	}
 
 
 	void OnGUI(){
@@ -25,7 +59,7 @@ public class highscore : MonoBehaviour {
 		GUI.Label(new Rect (10,120,200,80),"S.NO.");
 		GUI.Label(new Rect (150,120,250,80),"Playername");
 		GUI.Label(new Rect (390,120,200,80),"Score");
-		GUI.Label(new Rect (540,120,200,80),"Distance");
+		GUI.Label(new Rect (540,120,200,80),"Average");
 		GUI.Label(new Rect (740, 120, 200, 80), "LeftTurn");
 		GUI.Label(new Rect (980, 120, 200, 80), "RightTurn");
 		GUI.Label(new Rect (1210, 120, 200, 80), "TimePlay");
@@ -35,49 +69,46 @@ public class highscore : MonoBehaviour {
 			Application.LoadLevel("start");
 		}
 		GUI.Label(new Rect (30,190,120,60),"1.");
-		GUI.Label(new Rect (160,190,250,60),coindelete.Line[0]);
-		//Debug.Log(coindelete.Line[0]);
-		GUI.Label(new Rect (410,190,200,60),coindelete.Line[1]);
-		GUI.Label(new Rect (570,190,200,60),coindelete.Line[2]);
-		GUI.Label(new Rect (760,190, 200,60), coindelete.Line [3]);
-		GUI.Label(new Rect(990, 190, 200,60),coindelete.Line[4]);
-		GUI.Label(new Rect(1240, 190, 200,60),coindelete.Line[5]);
-		GUI.Label(new Rect(1450, 190, 450,60),coindelete.Line[6]);
+		GUI.Label(new Rect (160,190,250,60),Line[0]);
+		//Debug.Log(Line[0]);
+		GUI.Label(new Rect (410,190,200,60),Line[1]);
+		GUI.Label(new Rect (570,190,200,60),Line[2]);
+		GUI.Label(new Rect (760,190, 200,60), Line [3]);
+		GUI.Label(new Rect(990, 190, 200,60),Line[4]);
+		GUI.Label(new Rect(1240, 190, 200,60),Line[5]);
+		GUI.Label(new Rect(1450, 190, 450,60),Line[6]);
 		GUI.Label(new Rect (30,260,120,60),"2.");
-		GUI.Label(new Rect (160,260,250,60),coindelete.Line[7]);
-		GUI.Label(new Rect (410,260,200,60),coindelete.Line[8]);
-		GUI.Label(new Rect (570,260,200,60),coindelete.Line[9]);
-		GUI.Label(new Rect (760, 260, 200, 60), coindelete.Line [10]);
-		GUI.Label (new Rect (990, 260, 200, 60), coindelete.Line [11]);
-		GUI.Label(new Rect(1240, 260, 200,60),coindelete.Line[12]);
-		GUI.Label(new Rect(1450, 260, 400,60),coindelete.Line[13]);
+		GUI.Label(new Rect (160,260,250,60),Line[7]);
+		GUI.Label(new Rect (410,260,200,60),Line[8]);
+		GUI.Label(new Rect (570,260,200,60),Line[9]);
+		GUI.Label(new Rect (760, 260, 200, 60), Line [10]);
+		GUI.Label (new Rect (990, 260, 200, 60), Line [11]);
+		GUI.Label(new Rect(1240, 260, 200,60),Line[12]);
+		GUI.Label(new Rect(1450, 260, 400,60),Line[13]);
 		GUI.Label(new Rect (30,330,120,60),"3.");
-		GUI.Label(new Rect (160,330,250,60),coindelete.Line[14]);
-		GUI.Label(new Rect (410,330,200,60),coindelete.Line[15]);
-		GUI.Label(new Rect (570,330,200,60),coindelete.Line[16]);
-		GUI.Label (new Rect(760, 330, 200,60),coindelete.Line[17]);
-		GUI.Label(new Rect( 990,330,200,60),coindelete.Line[18]);
-		GUI.Label(new Rect(1240, 330, 200,60),coindelete.Line[19]);
-		GUI.Label(new Rect(1450, 330, 400,60),coindelete.Line[20]);
+		GUI.Label(new Rect (160,330,250,60),Line[14]);
+		GUI.Label(new Rect (410,330,200,60),Line[15]);
+		GUI.Label(new Rect (570,330,200,60),Line[16]);
+		GUI.Label (new Rect(760, 330, 200,60),Line[17]);
+		GUI.Label(new Rect( 990,330,200,60),Line[18]);
+		GUI.Label(new Rect(1240, 330, 200,60),Line[19]);
+		GUI.Label(new Rect(1450, 330, 400,60),Line[20]);
 		GUI.Label(new Rect (30,400,120,60),"4.");
-		GUI.Label(new Rect (160,400,250,60),coindelete.Line[21]);
-		GUI.Label(new Rect (410,400,200,60),coindelete.Line[22]);
-		GUI.Label(new Rect (570,400,200,60),coindelete.Line[23]);
-		GUI.Label (new Rect (760, 400, 200,60),coindelete.Line [24]);
-		GUI.Label (new Rect (990, 400, 200, 60), coindelete.Line [25]);
-		GUI.Label(new Rect(1240, 400, 200,60),coindelete.Line[26]);
-		GUI.Label(new Rect(1450, 400, 400,60),coindelete.Line[27]);
+		GUI.Label(new Rect (160,400,250,60),Line[21]);
+		GUI.Label(new Rect (410,400,200,60),Line[22]);
+		GUI.Label(new Rect (570,400,200,60),Line[23]);
+		GUI.Label (new Rect (760, 400, 200,60),Line [24]);
+		GUI.Label (new Rect (990, 400, 200, 60), Line [25]);
+		GUI.Label(new Rect(1240, 400, 200,60),Line[26]);
+		GUI.Label(new Rect(1450, 400, 400,60),Line[27]);
 		GUI.Label(new Rect (30,470,120,60),"5.");
-		GUI.Label(new Rect (160,470,250,60),coindelete.Line[28]);
-		GUI.Label(new Rect (410,470,200,60),coindelete.Line[29]);
-		GUI.Label(new Rect (570,470,200,60),coindelete.Line[30]);
-		GUI.Label (new Rect (760, 470, 200, 60), coindelete.Line [31]);
-		GUI.Label (new Rect (990, 470, 200, 60), coindelete.Line [32]);
-		GUI.Label(new Rect(1240, 470, 200,60),coindelete.Line[33]);
-		GUI.Label (new Rect (1450, 470, 400, 60), coindelete.Line [34]);
-       
-
-
-
-}
+		GUI.Label(new Rect (160,470,250,60),Line[28]);
+		GUI.Label(new Rect (410,470,200,60),Line[29]);
+		GUI.Label(new Rect (570,470,200,60),Line[30]);
+		GUI.Label (new Rect (760, 470, 200, 60), Line [31]);
+		GUI.Label (new Rect (990, 470, 200, 60), Line [32]);
+		GUI.Label(new Rect(1240, 470, 200,60),Line[33]);
+		GUI.Label (new Rect (1450, 470, 400, 60), Line [34]);
+    
+	}
 }
